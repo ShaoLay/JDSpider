8372e8e4a29b8633b41080471bea3c181fcb3197
lay <javs_shao@163.com>
2019-10-27 16:58:11 +0800

    管道文件编写

--- Book/Book/pipelines.py
+++ Book/Book/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exporters import JsonItemExporter
 
 
 class BookPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('book.json', 'wb')
+        self.write = JsonItemExporter(self.file)
+        self.write.start_exporting()
+
     def process_item(self, item, spider):
+        self.write.export_item(item)
         return item
+
+    def close_spider(self, spider):
+        self.write.finish_exporting()
+        self.file.close()
