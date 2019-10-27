91993ba699173086d5cda632447e1576cf465566
lay <javs_shao@163.com>
2019-10-27 16:31:42 +0800

    编写items文件

--- Book/Book/items.py
+++ Book/Book/items.py
@@ -11,4 +11,14 @@ import scrapy
 class BookItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    big_name = scrapy.Field()
+    samll_name = scrapy.Field()
+    small_url = scrapy.Field()
+    book_image = scrapy.Field()
+    book_name = scrapy.Field()
+    book_author = scrapy.Field()
+    book_store = scrapy.Field()
+    book_id = scrapy.Field()
+    book_time = scrapy.Field()
+    book_price = scrapy.Field()
+
--- Book/Book/spiders/book.py
+++ Book/Book/spiders/book.py
@@ -5,7 +5,7 @@ import scrapy
 class BookSpider(scrapy.Spider):
     name = 'book'
     allowed_domains = ['jd.com']
-    start_urls = ['http://jd.com/']
+    start_urls = ['https://book.jd.com/booksort.html']
 
     def parse(self, response):
         pass
